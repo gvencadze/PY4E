7e42026b06635b43b6973642b5a7605c21edd51b
Konstantin Gvencadze <48713403+gvencadze@users.noreply.github.com>
2019-04-01 15:14:05 +0300

    Update assignment8.4.py

--- w08/assignment8.4.py
+++ w08/assignment8.4.py
@@ -1,12 +1,9 @@
-#8.4 Open the file romeo.txt and read it line by line. For each line, split the line into a list of words using the split() function. The program should build a list of words. For each word on each line check to see if the word is already in the list and if not append it to the list. When the program completes, sort and print the resulting words in alphabetical order.
-#You can download the sample data at http://www.pythonlearn.com/code/romeo.txt
-
 def openfile():
-    fname = raw_input("Enter file name: ")
+    fname = raw_input("Enter file name: ") #название файла: romeo.txt
     try:
         fh = open(fname, 'r')
     except:
-        print "Error opening file", fname
+        print ("Error opening file", fname)
         quit()
     return fh
 
@@ -23,4 +20,4 @@ def listWords(fh):
 
 fh = openfile()
 result = listWords(fh)
-print result
+print (result)
