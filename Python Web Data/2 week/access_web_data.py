5b4fbaf8dd2834cdb7a1cf2c4aa32739880592e1
Konstantin Gvencadze <48713403+gvencadze@users.noreply.github.com>
2019-04-01 19:00:13 +0300

    Create access_web_data.py

---
+++ Python Web Data/2 week/access_web_data.py
@@ -0,0 +1,16 @@
+import re
+
+hand = open("week 1 sample text.txt")
+numlist = []
+
+for line in hand:
+    line = line.rstrip()
+    extract = re.findall("([0-9]+)", line)
+
+    if len(extract) < 1 : continue
+
+    for i in range(len(extract)):
+        num = float(extract[i])
+        numlist.append(num)
+
+print(sum(numlist))
