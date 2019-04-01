fb6289ecfe183c3101b62d85a8c2b2fdb984551a
Konstantin Gvencadze <48713403+gvencadze@users.noreply.github.com>
2019-04-01 15:27:14 +0300

    Update assignment9.4.py

--- w09/assignment9.4.py
+++ w09/assignment9.4.py
@@ -1,14 +1,11 @@
-#9.4 Write a program to read through the mbox-short.txt and figure out who has the most commits. The program looks for 'From ' lines and takes the second word of those lines as the person who sent the mail. The program creates a Python dictionary that maps the senders mail address to a count of the number of times they appear in the file. After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
-
-#You can download the sample data at http://www.pythonlearn.com/code/mbox-short.txt
-
+#название файла: mbox-short.txt , префикс: 'from'
 def openFile():
     fname = raw_input("Enter file name: ")
     if len(fname) < 1 : fname = "mbox-short.txt"
     try:
         fh = open(fname, 'r')
     except:
-        print "Error opening file", fname
+        print ("Error opening file", fname)
         quit()
     return fh
 
@@ -41,4 +38,4 @@ sw = startsWith()
 
 dictionary = doCount(fh,sw)
 key = max(dictionary)
-print key, dictionary[key] 
+print (key, dictionary[key])
