55b0cb715a91539506da5533f1f9874055419b4a
Konstantin Gvencadze <48713403+gvencadze@users.noreply.github.com>
2019-04-01 14:52:14 +0300

    Update assignment3.3.py

--- w03/assignment3.3.py
+++ w03/assignment3.3.py
@@ -1,11 +1,4 @@
-#3.3 Write a program to prompt the user for a score using raw_input. Print out a letter grade based on the following table:
-# Score Grade
-# >= 0.9 A
-# >= 0.8 B
-# >= 0.7 C
-# >= 0.6 D
-# < 0.6 F
-#If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
+#для проверки надо написать во всплывающее окно 0.85
 
 try:
     inp = raw_input("Please enter a score: ")
@@ -15,14 +8,14 @@ except:
     quit()
 
 if 0.0 <= score  < 0.6 :
-    print "F"
+    print ("F")
 elif 1.0 >= score >= 0.9 :
-    print "A"
+    print ("A")
 elif 0.9 > score >= 0.8 :
-    print "B"
+    print ("B")
 elif 0.8 > score >= 0.7 :
-    print "C"
+    print ("C")
 elif 0.7 > score >= 0.6 :
-    print "D"
+    print ("D")
 else :
-    print "Your score number is not in the 0 - 1 range."
+    print ("Your score number is not in the 0 - 1 range.")
